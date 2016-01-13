4ae10b7e66833f6c1521d32cb0db8e541f9f1dca
Andrei Vasiliu <andrei.vasiliu@intel.com>
2016-01-13 13:11:36 +0200

    Java: Solved unused variable warnings in BluetoothEvent.cxx
    Events are not yet implemented in the native part. This commit
    solves warning issues caused by unused variables from
    BluetoothEvent.cxx.
    
    Signed-off-by: Andrei Vasiliu <andrei.vasiliu@intel.com>

--- java/jni/BluetoothEvent.cxx
+++ java/jni/BluetoothEvent.cxx
@@ -2,31 +2,61 @@
 
 jobject Java_BluetoothEvent_getType(JNIEnv *env, jobject obj)
 {
+    (void)env;
+    (void)obj;
+
+    return NULL;
 }
 
 jstring Java_BluetoothEvent_getName(JNIEnv *env, jobject obj)
 {
+    (void)env;
+    (void)obj;
+
+    return NULL;
 }
 
 jstring Java_BluetoothEvent_getIdentifier(JNIEnv *env, jobject obj)
 {
+    (void)env;
+    (void)obj;
+
+    return NULL;
 }
 
 jboolean Java_BluetoothEvent_executeCallback(JNIEnv *env, jobject obj)
 {
+    (void)env;
+    (void)obj;
+
+    return JNI_FALSE;
 }
 
 jboolean Java_BluetoothEvent_hasCallback(JNIEnv *env, jobject obj)
 {
+    (void)env;
+    (void)obj;
+
+    return JNI_FALSE;
 }
 
 void Java_BluetoothEvent_init(JNIEnv *env, jobject obj, jobject type, jstring name,
                                 jstring identifier, jobject parent, jobject callback,
                                 jobject arg_data)
 {
+    (void)env;
+    (void)obj;
+    (void)type;
+    (void)name;
+    (void)identifier;
+    (void)parent;
+    (void)callback;
+    (void)arg_data;
 }
 
 void Java_BluetoothEvent_delete(JNIEnv *env, jobject obj)
 {
+    (void)env;
+    (void)obj;
 }
 
