5d2f8f092ea0762786a4ba6cea27f794b35176e9
Petre Eftime <petre.p.eftime@intel.com>
2016-07-04 16:44:53 +0300

    examples: simplify and complete error handling
    Signed-off-by: Petre Eftime <petre.p.eftime@intel.com>

--- examples/asynctinyb.cpp
+++ examples/asynctinyb.cpp
@@ -27,8 +27,12 @@
 #include <vector>
 #include <iostream>
 #include <thread>
+#include <atomic>
+#include <csignal>
 
 using namespace tinyb;
+
+
 
 /** Converts a raw temperature read from the sensor to a Celsius value.
  * @param[in] raw_temp The temperature read from the sensor (two bytes)
@@ -38,6 +42,15 @@ static float celsius_temp(uint16_t raw_temp)
 {
     const float SCALE_LSB = 0.03125;
     return ((float)(raw_temp >> 2)) * SCALE_LSB;
+}
+
+std::atomic<bool> running(true);
+
+void signal_handler(int signum)
+{
+    if (signum == SIGINT) {
+        running = false;
+    }
 }
 
 /** This program reads the temperature from a
@@ -72,17 +85,17 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    if (sensor_tag != nullptr) {
-        /* Connect to the device and get the list of services exposed by it */
-        sensor_tag->connect();
-        std::string service_uuid("f000aa00-0451-4000-b000-000000000000");
-        std::cout << "Waiting for service " << service_uuid << "to be discovered" << std::endl; 
-        temperature_service = sensor_tag->find(&service_uuid);
-    } else {
+    if (sensor_tag == nullptr) {
        ret = manager->stop_discovery();
        std::cerr << "SensorTag not found after 30 seconds, exiting" << std::endl;
        return 1;
     }
+
+    /* Connect to the device and get the list of services exposed by it */
+    sensor_tag->connect();
+    std::string service_uuid("f000aa00-0451-4000-b000-000000000000");
+    std::cout << "Waiting for service " << service_uuid << " to be discovered" << std::endl;
+    temperature_service = sensor_tag->find(&service_uuid);
 
     /* Stop the discovery (the device was found or timeout was over) */
     ret = manager->stop_discovery();
@@ -98,34 +111,51 @@ int main(int argc, char **argv)
     auto temp_period = temperature_service->find(&period_uuid);
 
     /* Activate the temperature measurements */
-    std::vector<unsigned char> config_on {0x01};
-    temp_config->write_value(config_on);
-    while (true) {
+    try {
+        std::vector<unsigned char> config_on {0x01};
+        temp_config->write_value(config_on);
+        std::signal(SIGINT, signal_handler);
+    } catch (std::exception &e) {
+         std::cout << "Error: " << e.what() << std::endl;
+         running = false;
+    }
+
+    while (running) {
         /* Read temperature data and display it */
-        std::vector<unsigned char> response = temp_value->read_value();
-        unsigned char *data;
-        unsigned int size = response.size();
-        if (size > 0) {
-            data = response.data();
+        try {
+            std::vector<unsigned char> response = temp_value->read_value();
+            unsigned char *data;
+            unsigned int size = response.size();
+            if (size > 0) {
+                data = response.data();
 
-            std::cout << "Raw data=[";
-            for (unsigned i = 0; i < response.size(); i++)
-                std::cout << std::hex << static_cast<int>(data[i]) << ", ";
-            std::cout << "] ";
+                std::cout << "Raw data=[";
+                for (unsigned i = 0; i < response.size(); i++)
+                    std::cout << std::hex << static_cast<int>(data[i]) << ", ";
+                std::cout << "] ";
 
-            uint16_t ambient_temp, object_temp;
-            object_temp = data[0] | (data[1] << 8);
-            ambient_temp = data[2] | (data[3] << 8);
+                uint16_t ambient_temp, object_temp;
+                object_temp = data[0] | (data[1] << 8);
+                ambient_temp = data[2] | (data[3] << 8);
 
-            std::cout << "Ambient temp: " << celsius_temp(ambient_temp) << "C ";
-            std::cout << "Object temp: " << celsius_temp(object_temp) << "C ";
-            std::cout << std::endl;
+                std::cout << "Ambient temp: " << celsius_temp(ambient_temp) << "C ";
+                std::cout << "Object temp: " << celsius_temp(object_temp) << "C ";
+                std::cout << std::endl;
+            }
+
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+
+        } catch (std::exception &e) {
+            std::cout << "Error: " << e.what() << std::endl;
+            break;
         }
-
-        std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 
     /* Disconnect from the device */
-    if (sensor_tag != NULL)
+    try {
         sensor_tag->disconnect();
+    } catch (std::exception &e) {
+        std::cout << "Error: " << e.what() << std::endl;
+    }
+    return 0;
 }
