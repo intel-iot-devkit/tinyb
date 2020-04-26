856a3fcf411dfa3570760bb9ae6a04139f362418
Sven Gothel <sgothel@jausoft.com>
2020-04-26 14:10:20 +0200

    test clang++ compiler script fix

--- scripts/clangppone.sh
+++ scripts/clangppone.sh
@@ -3,5 +3,5 @@
 sdir=`dirname $(readlink -f $0)`
 rootdir=`dirname $sdir`
 
-clear ; clang++ -c -o a.o -std=c++11 -Wall -I$rootdir/api/direct_bt -I$rootdir/ieee11073 -I$rootdir/api $1 2>&1 | tee make.log
+clang++ -c -o a.o -std=c++11 -Wall -I$rootdir/api/direct_bt -I$rootdir/ieee11073 -I$rootdir/api $1 2>&1 | tee make.log
 rm -f a.o
