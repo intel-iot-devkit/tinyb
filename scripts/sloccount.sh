db2b55a1e99130f7e0c840156c453744fa7cb24f
Sven Gothel <sgothel@jausoft.com>
2020-05-17 11:39:45 +0200

    sloccount: Adding java/jni and test code

--- scripts/sloccount.sh
+++ scripts/sloccount.sh
@@ -12,8 +12,12 @@ cd ..
 # ln -s ../api/direct_bt api
 
 ln -s ../include/cppunit cppunit
-ln -s ../src/direct_bt src
+ln -s ../src/direct_bt src_cpp
 ln -s ../test/direct_bt test
+ln -s ../java/direct_bt src_java
+ln -s ../java/jni/direct_bt src_jni
+ln -s ../test src_test
 cd ..
 
 sloccount --follow --personcost 100000 --overhead 1.30 sloccount_direct_bt 2>&1 | tee sloccount_direct_bt-`date +%Y%m%d`.log
+#sloccount --details --wide --follow --personcost 100000 --overhead 1.30 sloccount_direct_bt 2>&1 | tee sloccount_direct_bt-`date +%Y%m%d`.log
