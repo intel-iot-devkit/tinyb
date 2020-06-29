intel-iot-devkit/tinyb

commit 83e0c7f75b4206701c1b9c62ebd4282f50f92a31

Notify DBTNativeDownlink when its native JavaUplink -> JavaGlobalObj counterpart gets destructed
DBTNativeDownlink.notifyDeleted() gets called from native destructor,
so isValid and nativeInstance can be set accordingly.

This resolves a periodic crash when GATTHandler flushes it's GATTServices and linked resources.
Here the DBTGATTService.java's DBTNativeDownlink still holds the native instance handle
and as it gets finalized after the native object, accessing it post mortem causes a SIGSEGV.

java/direct_bt/tinyb/DBTNativeDownlink.java
@@ -64,8 +64,18 @@ public abstract class DBTNativeDownlink
             return;
         }
         isValid = false;
-        clearNativeJavaObject(nativeInstance);
+        deleteNativeJavaObject(nativeInstance);
         deleteImpl();
+        nativeInstance = 0;
+    }
+
+    /**
+     * Called from native JavaUplink dtor -> JavaGlobalObj dtor,
+     * i.e. native instance destructed in native land.
+     */
+    private synchronized void notifyDeleted() {
+        // System.err.println("***** notifyDeleted: "+getClass().getSimpleName()+": valid "+isValid+" -> false, handle 0x"+Long.toHexString(nativeInstance)+" -> null");
+        isValid = false;
         nativeInstance = 0;
     }
 
@@ -79,5 +89,5 @@ public abstract class DBTNativeDownlink
     protected abstract void deleteImpl();
 
     private native void initNativeJavaObject(final long nativeInstance);
-    private native void clearNativeJavaObject(final long nativeInstance);
+    private native void deleteNativeJavaObject(final long nativeInstance);
 }

java/jni/direct_bt/DBTDevice.cxx
@@ -252,6 +252,7 @@ void Java_direct_1bt_tinyb_DBTDevice_deleteImpl(JNIEnv *env, jobject obj)
         DBTDevice *device = getInstance<DBTDevice>(env, obj);
         device->remove();
         // No delete: DBTDevice instance owned by DBTAdapter
+        // However, device->remove() might issue destruction
     } catch(...) {
         rethrow_and_raise_java_exception(env);
     }

java/jni/direct_bt/DBTManager.cxx
@@ -45,7 +45,7 @@ void Java_direct_1bt_tinyb_DBTManager_initImpl(JNIEnv *env, jobject obj, jboolea
         DBTManager *manager = &DBTManager::get(BTMode::BT_MODE_LE); // special: static singleton
         setInstance<DBTManager>(env, obj, manager);
         java_exception_check_and_throw(env, E_FILE_LINE);
-        manager->setJavaObject( std::shared_ptr<JavaAnonObj>( new JavaGlobalObj(obj) ) );
+        manager->setJavaObject( std::shared_ptr<JavaAnonObj>( new JavaGlobalObj(obj, nullptr) ) );
         JavaGlobalObj::check(manager->getJavaObject(), E_FILE_LINE);
         DBG_PRINT("Java_direct_1bt_tinyb_DBTManager_init: Manager %s", manager->toString().c_str());
     } catch(...) {

java/jni/direct_bt/DBTNativeDownlink.cxx
@@ -40,7 +40,20 @@ void Java_direct_1bt_tinyb_DBTNativeDownlink_initNativeJavaObject(JNIEnv *env, j
 {
     try {
         JavaUplink *javaUplink = castInstance<JavaUplink>(nativeInstance);
-        std::shared_ptr<JavaGlobalObj> jobjRef( new JavaGlobalObj(obj) );
+        if( nullptr == javaUplink ) {
+            throw InternalError("NativeInstance JavaUplink is NULL", E_FILE_LINE);
+        }
+        jclass javaClazz = search_class(env, obj);
+        java_exception_check_and_throw(env, E_FILE_LINE);
+        if( nullptr == javaClazz ) {
+            throw InternalError("DBTNativeDownlink class not found", E_FILE_LINE);
+        }
+        jmethodID  mNotifyDeleted = search_method(env, javaClazz, "notifyDeleted", "()V", false);
+        java_exception_check_and_throw(env, E_FILE_LINE);
+        if( nullptr == mNotifyDeleted ) {
+            throw InternalError("DBTNativeDownlink class has no notifyDeleted() method, for "+javaUplink->toString(), E_FILE_LINE);
+        }
+        std::shared_ptr<JavaGlobalObj> jobjRef( new JavaGlobalObj(obj, mNotifyDeleted) );
         javaUplink->setJavaObject( jobjRef );
         JavaGlobalObj::check(javaUplink->getJavaObject(), E_FILE_LINE);
         DBG_PRINT("Java_direct_1bt_tinyb_DBTNativeDownlink_initNativeJavaObject %p -> %s", javaUplink, javaUplink->toString().c_str());
@@ -49,12 +62,15 @@ void Java_direct_1bt_tinyb_DBTNativeDownlink_initNativeJavaObject(JNIEnv *env, j
     }
 }
 
-void Java_direct_1bt_tinyb_DBTNativeDownlink_clearNativeJavaObject(JNIEnv *env, jobject obj, jlong nativeInstance)
+void Java_direct_1bt_tinyb_DBTNativeDownlink_deleteNativeJavaObject(JNIEnv *env, jobject obj, jlong nativeInstance)
 {
     (void)obj;
     try {
         JavaUplink *javaUplink = castInstance<JavaUplink>(nativeInstance);
-        DBG_PRINT("Java_direct_1bt_tinyb_DBTNativeDownlink_clearNativeJavaObject %p -> %s", javaUplink, javaUplink->toString().c_str());
+        if( nullptr == javaUplink ) {
+            throw InternalError("NativeInstance JavaUplink is NULL", E_FILE_LINE);
+        }
+        DBG_PRINT("Java_direct_1bt_tinyb_DBTNativeDownlink_deleteNativeJavaObject %p -> %s", javaUplink, javaUplink->toString().c_str());
         javaUplink->setJavaObject(nullptr);
     } catch(...) {
         rethrow_and_raise_java_exception(env);

java/jni/direct_bt/helper_dbt.cxx
@@ -67,3 +67,14 @@ jstring direct_bt::fromBDAddressTypeToJavaAddressType(JNIEnv *env, BDAddressType
             return from_string_to_jstring(env, jStringEmpty);
     }
 }
+
+JavaGlobalObj::~JavaGlobalObj() {
+    jobject obj = javaObjectRef.getObject();
+    if( nullptr == obj || nullptr == mNotifyDeleted ) {
+        return;
+    }
+    JNIEnv *env = *jni_env;
+    env->CallVoidMethod(obj, mNotifyDeleted);
+    java_exception_check_and_throw(env, E_FILE_LINE);
+}
+

java/jni/direct_bt/helper_dbt.hpp
@@ -59,6 +59,7 @@ namespace direct_bt {
     class JavaGlobalObj : public JavaAnonObj {
         private:
             JNIGlobalRef javaObjectRef;
+            jmethodID  mNotifyDeleted;
 
         public:
             static inline void check(const std::shared_ptr<JavaAnonObj> & shref, const char* file, int line) {
@@ -80,12 +81,15 @@ namespace direct_bt {
                 }
                 return true;
             }
-            JavaGlobalObj(jobject obj) : javaObjectRef(obj) { }
+            JavaGlobalObj(jobject obj, jmethodID mNotifyDeleted)
+            : javaObjectRef(obj), mNotifyDeleted(mNotifyDeleted) { }
 
             JavaGlobalObj(const JavaGlobalObj &o) noexcept = default;
             JavaGlobalObj(JavaGlobalObj &&o) noexcept = default;
             JavaGlobalObj& operator=(const JavaGlobalObj &o) noexcept = default;
             JavaGlobalObj& operator=(JavaGlobalObj &&o) noexcept = default;
+
+            virtual ~JavaGlobalObj();
 
             std::string toString() const override {
                 const uint64_t ref = (uint64_t)(void*)javaObjectRef.getObject();
@@ -112,7 +116,6 @@ namespace direct_bt {
                 return static_cast<JavaGlobalObj*>(shref.get())->getClass();
             }
     };
-
 
     jclass search_class(JNIEnv *env, JavaUplink &object);
 

src/direct_bt/GATTHandler.cpp
@@ -267,7 +267,7 @@ bool GATTHandler::connect() {
         return true;
     }
     hasIOError = false;
-    INFO_PRINT("GATTHandler::connect: Start: GattHandler[%s], l2cap[%s]: %s",
+    DBG_PRINT("GATTHandler::connect: Start: GattHandler[%s], l2cap[%s]: %s",
                 getStateString().c_str(), l2cap.getStateString().c_str(), deviceString.c_str());
 
     if( !l2cap.connect() || !validateConnected() ) {

commit 0b7e94850a0bb5e4d389709bc9c449cd4f7dc094

Resolve regression calling DBTNativeDownlink.delete() having notifyDeleted() being called back from deleteNativeJavaObject(..)
Regression introduced with commit 83e0c7f75b4206701c1b9c62ebd4282f50f92a31

DBTNativeDownlink.delete() calls DBTNativeDownlink.deleteNativeJavaObject(..),
which by itself now calls back using DBTNativeDownlink.notifyDeleted() and purges isValid and nativeInstance.

Therefor, DBTNativeDownlink.delete() needs to cache the nativeInstance reference and pass it
down to DBTNativeDownlink.deleteImpl(long) as argument.

Further refine synchronization, using a local Object and isValid being an AtomicBoolean
to allow isValid() return the current state.

java/direct_bt/tinyb/DBTAdapter.java
@@ -303,7 +303,7 @@ public class DBTAdapter extends DBTObject implements BluetoothAdapter
     /* internal */
 
     @Override
-    protected native void deleteImpl();
+    protected native void deleteImpl(long nativeInstance);
 
     /* discovery */
 

java/direct_bt/tinyb/DBTDevice.java
@@ -597,7 +597,7 @@ public class DBTDevice extends DBTObject implements BluetoothDevice
      * </p>
      */
     @Override
-    protected native void deleteImpl();
+    protected native void deleteImpl(long nativeInstance);
 
     @Override
     public native boolean addCharacteristicListener(final GATTCharacteristicListener listener, final BluetoothGattCharacteristic characteristicMatch);

java/direct_bt/tinyb/DBTGattCharacteristic.java
@@ -322,6 +322,6 @@ public class DBTGattCharacteristic extends DBTObject implements BluetoothGattCha
     private native boolean enableValueNotificationsImpl(boolean v);
 
     @Override
-    protected native void deleteImpl();
+    protected native void deleteImpl(long nativeInstance);
 
 }

java/direct_bt/tinyb/DBTGattDescriptor.java
@@ -167,5 +167,5 @@ public class DBTGattDescriptor extends DBTObject implements BluetoothGattDescrip
     private native boolean writeValueImpl(byte[] argValue) throws BluetoothException;
 
     @Override
-    protected native void deleteImpl();
+    protected native void deleteImpl(long nativeInstance);
 }

java/direct_bt/tinyb/DBTGattService.java
@@ -131,5 +131,5 @@ public class DBTGattService extends DBTObject implements BluetoothGattService
     private native List<BluetoothGattCharacteristic> getCharacteristicsImpl();
 
     @Override
-    protected native void deleteImpl();
+    protected native void deleteImpl(long nativeInstance);
 }

java/direct_bt/tinyb/DBTManager.java
@@ -43,6 +43,7 @@ import org.tinyb.BluetoothType;
 public class DBTManager implements BluetoothManager
 {
     protected static final boolean DEBUG = BluetoothFactory.DEBUG;
+    protected static final boolean VERBOSE = BluetoothFactory.VERBOSE;
 
     private static volatile boolean isJVMShuttingDown = false;
     private static final List<Runnable> userShutdownHooks = new ArrayList<Runnable>();
@@ -269,7 +270,7 @@ public class DBTManager implements BluetoothManager
     private native List<BluetoothAdapter> getAdapterListImpl();
 
     private native void initImpl(final boolean unifyUUID128Bit) throws BluetoothException;
-    private native void deleteImpl();
+    private native void deleteImpl(long nativeInstance);
     private DBTManager()
     {
         initImpl(unifyUUID128Bit);
@@ -304,7 +305,7 @@ public class DBTManager implements BluetoothManager
             a.close();
         }
         adapters.clear();
-        deleteImpl();
+        deleteImpl(nativeInstance);
     }
 
 }

java/direct_bt/tinyb/DBTNativeDownlink.java
@@ -25,12 +25,15 @@
 
 package direct_bt.tinyb;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import org.tinyb.BluetoothFactory;
 
 public abstract class DBTNativeDownlink
 {
-    protected long nativeInstance;
-    private boolean isValid;
+    private long nativeInstance;
+    private final AtomicBoolean isValid = new AtomicBoolean(false);
+    private final Object nativeLock = new Object();
 
     static {
         BluetoothFactory.checkInitialized();
@@ -39,11 +42,11 @@ public abstract class DBTNativeDownlink
     protected DBTNativeDownlink(final long nativeInstance)
     {
         this.nativeInstance = nativeInstance;
-        isValid = true;
+        isValid.set(true);
         initNativeJavaObject(nativeInstance);
     }
 
-    protected final boolean isValid() { return isValid; }
+    protected final boolean isValid() { return isValid.get(); }
 
     @Override
     protected void finalize()
@@ -52,41 +55,62 @@ public abstract class DBTNativeDownlink
     }
 
     /**
-     * Deletes the native instance in the following order
+     * Deletes the {@code nativeInstance} in the following order
      * <ol>
-     *   <li>Removes this java reference from the native instance</li>
-     *   <li>Deletes the native instance via {@link #deleteImpl()}</li>
-     *   <li>Sets the nativeInstance := 0</li>
+     *   <li>Removes this java reference from the {@code nativeInstance}</li>
+     *   <li>Deletes the {@code nativeInstance} via {@link #deleteImpl(long)}</li>
+     *   <li>Zeros the {@code nativeInstance} reference</li>
      * </ol>
      */
-    public synchronized void delete() {
-        if (!isValid) {
-            return;
+    public final void delete() {
+        synchronized (nativeLock) {
+            if( !isValid.compareAndSet(true, false) ) {
+                if( DBTManager.DEBUG ) {
+                    System.err.println("JAVA: delete: !valid -> bail: "+getClass().getSimpleName());
+                }
+                return;
+            }
+            if( DBTManager.DEBUG ) {
+                System.err.println("JAVA: delete.0: "+getClass().getSimpleName()+": valid, handle 0x"+Long.toHexString(nativeInstance));
+            }
+            final long _nativeInstance = nativeInstance;
+            nativeInstance = 0;
+            deleteNativeJavaObject(_nativeInstance); // will issue notifyDeleted() itself!
+            deleteImpl(_nativeInstance);
+            if( DBTManager.DEBUG ) {
+                System.err.println("JAVA: delete.X: "+getClass().getSimpleName()+": handle 0x"+Long.toHexString(nativeInstance));
+            }
         }
-        isValid = false;
-        deleteNativeJavaObject(nativeInstance);
-        deleteImpl();
-        nativeInstance = 0;
     }
 
     /**
      * Called from native JavaUplink dtor -> JavaGlobalObj dtor,
      * i.e. native instance destructed in native land.
      */
-    private synchronized void notifyDeleted() {
-        isValid = false;
-        nativeInstance = 0;
-        // System.err.println("***** notifyDeleted: "+getClass().getSimpleName()+": valid "+isValid+" -> false, handle 0x"+Long.toHexString(nativeInstance)+" -> null: "+toString());
+    private final void notifyDeleted() {
+        synchronized (nativeLock) {
+            final boolean _isValid = isValid.get();
+            final long _nativeInstance = nativeInstance;
+            isValid.set(false);
+            nativeInstance = 0;
+            if( DBTManager.DEBUG ) {
+                System.err.println("JAVA: delete.notifyDeleted: "+getClass().getSimpleName()+", was: valid "+_isValid+", handle 0x"+Long.toHexString(_nativeInstance)+": "+toString());
+            }
+        }
     }
 
     /**
      * Deletes the native instance.
      * <p>
-     * Called via {@link #delete()} and at this point this java reference
-     * has been removed from the native instance.
+     * Called via {@link #delete()} and at this point
+     * <ul>
+     *  <li>this java reference has been removed from the native instance, i.e. {@code JavaUplink}'s {@code javaObjectRef = nullptr}</li>
+     *  <li>the {@link #nativeInstance} reference has been zeroed, but passed as argument for this final native deletion task.</li>
+     * </ul>
      * </p>
+     * @param nativeInstance copy of {@link #nativeInstance} reference, which has been already zeroed.
      */
-    protected abstract void deleteImpl();
+    protected abstract void deleteImpl(long nativeInstance);
 
     private native void initNativeJavaObject(final long nativeInstance);
     private native void deleteNativeJavaObject(final long nativeInstance);

java/jni/direct_bt/DBTAdapter.cxx
@@ -507,10 +507,11 @@ jstring Java_direct_1bt_tinyb_DBTAdapter_toStringImpl(JNIEnv *env, jobject obj) 
     return nullptr;
 }
 
-void Java_direct_1bt_tinyb_DBTAdapter_deleteImpl(JNIEnv *env, jobject obj)
+void Java_direct_1bt_tinyb_DBTAdapter_deleteImpl(JNIEnv *env, jobject obj, jlong nativeInstance)
 {
+    (void)obj;
     try {
-        DBTAdapter *adapter = getInstance<DBTAdapter>(env, obj);
+        DBTAdapter *adapter = castInstance<DBTAdapter>(nativeInstance);
         DBG_PRINT("Java_direct_1bt_tinyb_DBTAdapter_deleteImpl %s", adapter->toString().c_str());
         delete adapter;
     } catch(...) {

java/jni/direct_bt/DBTDevice.cxx
@@ -246,10 +246,11 @@ jint Java_direct_1bt_tinyb_DBTDevice_removeAllCharacteristicListener(JNIEnv *env
     return 0;
 }
 
-void Java_direct_1bt_tinyb_DBTDevice_deleteImpl(JNIEnv *env, jobject obj)
+void Java_direct_1bt_tinyb_DBTDevice_deleteImpl(JNIEnv *env, jobject obj, jlong nativeInstance)
 {
+    (void)obj;
     try {
-        DBTDevice *device = getInstance<DBTDevice>(env, obj);
+        DBTDevice *device = castInstance<DBTDevice>(nativeInstance);
         device->remove();
         // No delete: DBTDevice instance owned by DBTAdapter
         // However, device->remove() might issue destruction

java/jni/direct_bt/DBTGattCharacteristic.cxx
@@ -48,9 +48,10 @@ jstring Java_direct_1bt_tinyb_DBTGattCharacteristic_toStringImpl(JNIEnv *env, jo
     return nullptr;
 }
 
-void Java_direct_1bt_tinyb_DBTGattCharacteristic_deleteImpl(JNIEnv *env, jobject obj) {
+void Java_direct_1bt_tinyb_DBTGattCharacteristic_deleteImpl(JNIEnv *env, jobject obj, jlong nativeInstance) {
+    (void)obj;
     try {
-        GATTCharacteristic *characteristic = getInstance<GATTCharacteristic>(env, obj);
+        GATTCharacteristic *characteristic = castInstance<GATTCharacteristic>(nativeInstance);
         (void)characteristic;
         // No delete: Service instance owned by GATTService -> DBTDevice
     } catch(...) {

java/jni/direct_bt/DBTGattDescriptor.cxx
@@ -47,9 +47,10 @@ void Java_direct_1bt_tinyb_DBTGattDescriptor_deleteImpl(JNIEnv *env, jobject obj
     }
 }
 
-jstring Java_direct_1bt_tinyb_DBTGattDescriptor_toStringImpl(JNIEnv *env, jobject obj) {
+jstring Java_direct_1bt_tinyb_DBTGattDescriptor_toStringImpl(JNIEnv *env, jobject obj, jlong nativeInstance) {
+    (void)obj;
     try {
-        GATTDescriptor *nativePtr = getInstance<GATTDescriptor>(env, obj);
+        GATTDescriptor *nativePtr = castInstance<GATTDescriptor>(nativeInstance);
         JavaGlobalObj::check(nativePtr->getJavaObject(), E_FILE_LINE);
         return from_string_to_jstring(env, nativePtr->toString());
     } catch(...) {

java/jni/direct_bt/DBTGattService.cxx
@@ -49,9 +49,10 @@ jstring Java_direct_1bt_tinyb_DBTGattService_toStringImpl(JNIEnv *env, jobject o
 }
 
 
-void Java_direct_1bt_tinyb_DBTGattService_deleteImpl(JNIEnv *env, jobject obj) {
+void Java_direct_1bt_tinyb_DBTGattService_deleteImpl(JNIEnv *env, jobject obj, jlong nativeInstance) {
+    (void)obj;
     try {
-        GATTService *service = getInstance<GATTService>(env, obj);
+        GATTService *service = castInstance<GATTService>(nativeInstance);
         (void)service;
         // No delete: Service instance owned by DBTDevice
     } catch(...) {

java/jni/direct_bt/DBTManager.cxx
@@ -53,10 +53,11 @@ void Java_direct_1bt_tinyb_DBTManager_initImpl(JNIEnv *env, jobject obj, jboolea
     }
 }
 
-void Java_direct_1bt_tinyb_DBTManager_deleteImpl(JNIEnv *env, jobject obj)
+void Java_direct_1bt_tinyb_DBTManager_deleteImpl(JNIEnv *env, jobject obj, jlong nativeInstance)
 {
+    (void)obj;
     try {
-        DBTManager *manager = getInstance<DBTManager>(env, obj); // special: static singleton
+        DBTManager *manager = castInstance<DBTManager>(nativeInstance); // special: static singleton
         manager->close();
         manager->setJavaObject(nullptr);
         (void) manager;

java/jni/direct_bt/DBTNativeDownlink.cxx
@@ -40,9 +40,6 @@ void Java_direct_1bt_tinyb_DBTNativeDownlink_initNativeJavaObject(JNIEnv *env, j
 {
     try {
         JavaUplink *javaUplink = castInstance<JavaUplink>(nativeInstance);
-        if( nullptr == javaUplink ) {
-            throw InternalError("NativeInstance JavaUplink is NULL", E_FILE_LINE);
-        }
         jclass javaClazz = search_class(env, obj);
         java_exception_check_and_throw(env, E_FILE_LINE);
         if( nullptr == javaClazz ) {
@@ -67,9 +64,6 @@ void Java_direct_1bt_tinyb_DBTNativeDownlink_deleteNativeJavaObject(JNIEnv *env,
     (void)obj;
     try {
         JavaUplink *javaUplink = castInstance<JavaUplink>(nativeInstance);
-        if( nullptr == javaUplink ) {
-            throw InternalError("NativeInstance JavaUplink is NULL", E_FILE_LINE);
-        }
         DBG_PRINT("Java_direct_1bt_tinyb_DBTNativeDownlink_deleteNativeJavaObject %p -> %s", javaUplink, javaUplink->toString().c_str());
         javaUplink->setJavaObject(nullptr);
     } catch(...) {